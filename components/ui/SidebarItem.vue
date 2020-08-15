60490396814e5a497964464e5dd69b21d387954f
Keigo Nishida <keigo.nishida.1480@gmail.com>
2020-08-15 13:54:22 +0900

    #118 [add] created a new component for the sidebar items

---
+++ components/ui/SidebarItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list-item
+    link
+    :to="{
+      path: path,
+    }"
+    nuxt
+  >
+    <v-list-item-action>
+      <v-icon>
+        <slot name="icon" />
+      </v-icon>
+    </v-list-item-action>
+    <v-list-item-content>
+      <v-list-item-title>
+        <slot name="title" />
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    path: {
+      type: String,
+      default: '/',
+    },
+  },
+}
+</script>
